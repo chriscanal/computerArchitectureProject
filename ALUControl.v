b075089062a2e3387d718f915f57a186bc3f908b
chris <chris@CORP.AMAZONWORKSPACES.COM>
2016-04-10 13:57:44 -0400

    Fixed the ALUcontrol and added comments to identify each possible process

--- ALUControl.v
+++ ALUControl.v
@@ -7,21 +7,28 @@ module ALUControl (ALUOp, Funct, Operation);
   begin
     case (ALUOp)
       2'b00:
+	//lw,sw, addi, and addiu - add for load word, store word add immediate and add immediate unsigned
 	Operation = 4'b0010;
       2'b01:
+	//beq - subtract for branch if equal
 	Operation = 4'b0110;
       2'b10:
 	begin
 	  case (Funct[0 +: 4])
 	    4'b0000:
+	      //add
 	      Operation = 4'b0010;
 	    4'b0010:
+	      //sub - subtract
 	      Operation = 4'b0110;
 	    4'b0100:
+	      //AND
 	      Operation = 4'b0000;
 	    4'b0101:
+	      //OR
 	      Operation = 4'b0001;
 	    4'b1010:
+	      //slt - Set on less than
 	      Operation = 4'b0111;
 	  endcase
 	end
